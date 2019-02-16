31e356b089017bf3f21a5fc93d1cdee7c7a8397e
Daniel André da Silva <dap1995@gmail.com>
2019-02-16 16:43:48 -0200

    Refactor to list all stores and stores by distance

--- lib/spacestore_web/resolvers/business_resolver.ex
+++ lib/spacestore_web/resolvers/business_resolver.ex
@@ -11,8 +11,13 @@ defmodule SpacestoreWeb.BusinessResolver do
     end
   end
 
-  def all_stores(_root, args, _info) do
+  def stores_by_distance(_root, args, _info) do
     stores = Business.list_stores_by_distance(args, [:owner, :address])
+    {:ok, stores}
+  end
+
+  def all_stores(_root, %{}, _info) do
+    stores = Business.list_stores_with_preload([:owner, :address])
     {:ok, stores}
   end
 end
--- lib/spacestore_web/schema.ex
+++ lib/spacestore_web/schema.ex
@@ -25,16 +25,21 @@ defmodule SpacestoreWeb.Schema do
   object :store do
     field :id, non_null(:id)
     field :name, non_null(:string)
+    field :description, non_null(:string)
     field :email, non_null(:string)
     field :owner, non_null(:owner)
     field :address, non_null(:address)
   end
 
   query do
-    field :all_stores, non_null(list_of(non_null(:store))) do
+    field :stores, non_null(list_of(non_null(:store))) do
       arg :latitude, non_null(:float)
       arg :longitude, non_null(:float)
       arg :distance, :float, default_value: 10
+      resolve &BusinessResolver.stores_by_distance/3
+    end
+
+    field :all_stores, non_null(list_of(non_null(:store))) do
       resolve &BusinessResolver.all_stores/3
     end
   end
